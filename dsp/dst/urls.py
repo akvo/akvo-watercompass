12242c88eb6da8ca834e0bed53dbdc2621d10885
Daniel Karlsson <daniel@fraction4.com>
2010-01-28 15:39:35 +0100

    Added fancybox and updates the templates

--- dsp/dst/urls.py
+++ dsp/dst/urls.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^$', 'dst.views.grid', name='grid'),
     url(r'^(?P<model>\w+)_help/(?P<id>\d+)/$', 'dst.views.grid', name='grid_help'),  
     url(r'^(?P<model>\w+)_factorhelp/(?P<id>\d+)/$', 'dst.views.factor_help', name='factor_help'),
     
-    url(r'^technologies/$', 'dst.views.technologies', name='technologies'),
-    url(r'^solution/$', 'dst.views.solution', name='solution'),
+    url(r'^technologies$', 'dst.views.technologies', name='technologies'),
+    url(r'^solution$', 'dst.views.solution', name='solution'),
+        
+    url(r'^help$', direct_to_template, {'template': 'dst/help.html'}, name='help'),
+    url(r'^demo$', direct_to_template, {'template': 'dst/demo.html'}, name='demo'),
 )
