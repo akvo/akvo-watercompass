3cb062935c25ef7ed6db4e332e62ae94b9fe2942
Daniel Karlsson <daniel@fraction4.com>
2010-03-02 10:37:38 +0100

    Renamed grid url to factors.

--- dsp/dst/urls.py
+++ dsp/dst/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    url(r'^$', 'dst.views.grid', name='grid'),
+    url(r'^$', 'dst.views.grid', name='factors'),
     url(r'^(?P<model>\w+)_help/(?P<id>\d+)/$', 'dst.views.grid', name='grid_help'),  
     url(r'^(?P<model>\w+)_factorhelp/(?P<id>\d+)/$', 'dst.views.factor_help', name='factor_help'),
     
