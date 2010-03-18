63fc9d08dfd1f952e2e65946bca6a135910302bc
Daniel Karlsson <daniel@fraction4.com>
2010-03-18 14:20:56 +0000

    Changed technology img fields in the db and made a db dump

--- dsp/datadir/dst_techchoice.py
+++ dsp/datadir/dst_techchoice.py
@@ -4,4 +4,8 @@ fields = ['id', 'session_id', 'technology_id']
 default = {}
 records = [
 [1, u'17b9fca83761ab3d89f7392d9fb31917', 2]
+[4, u'f51b2302b164317f4f1378995464ce26', 4]
+[5, u'f51b2302b164317f4f1378995464ce26', 6]
+[6, u'64ef93f3af79a331aa3f0dc8b9f547d6', 7]
+[7, u'64ef93f3af79a331aa3f0dc8b9f547d6', 5]
 ]
