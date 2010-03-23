8b9045584ab47ff14fb79ea79020fa4f30a14539
Daniel Karlsson <daniel@fraction4.com>
2010-03-23 14:28:24 +0000

    Exported new data from the db

--- dsp/datadir/dst_technology_output.py
+++ dsp/datadir/dst_technology_output.py
@@ -3,14 +3,4 @@ fields = ['id', 'from_technology_id', 'to_technology_id']
 #default item format: "fieldname":("type", "value")
 default = {}
 records = [
-[2, 2, 4]
-[3, 3, 5]
-[5, 5, 7]
-[6, 6, 10]
-[8, 8, 10]
-[9, 4, 5]
-[10, 4, 6]
-[11, 7, 9]
-[12, 7, 10]
-[13, 1, 3]
 ]
