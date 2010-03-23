14f97933b60637d5f14ff4177016ce96b0147ade
Daniel Karlsson <daniel@fraction4.com>
2010-03-23 17:13:00 +0000

    Exported latest data

--- dsp/datadir/auth_user.py
+++ dsp/datadir/auth_user.py
@@ -3,9 +3,10 @@ fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'is_
 #default item format: "fieldname":("type", "value")
 default = {}
 records = [
-[1, u'gabriel', u'', u'', u'g@foo.com', u'sha1$4667d$3ed2200d6f4742f70e3459cc37cba829f5659c47', True, True, True, '2010-03-18 08:52:57', '2009-12-02 17:45:52']
+[1, u'gabriel', u'', u'', u'g@foo.com', u'sha1$4667d$3ed2200d6f4742f70e3459cc37cba829f5659c47', True, True, True, '2010-03-23 11:47:10', '2009-12-02 17:45:52']
 [2, u'daniel', u'Daniel', u'Karlsson', u'daniel@fraction4.com', u'sha1$7a632$714f463e9a8bbf07be04acc56c48434e3d58e2fc', True, True, True, '2010-03-23 09:14:01', '2010-02-17 10:04:00']
 [3, u'markw', u'Mark', u'Westra', u'm.t.westra@akvo.org', u'sha1$1fff8$276d57d42241aeb72ee094d216da72d4e8232193', True, True, True, '2010-03-23 04:27:14', '2010-03-10 04:17:21']
 [4, u'bethwaaa', u'Beth', u'Whiteside', u'beth@akvo.org', u'sha1$9e485$f4b0543afd20382d70bf9a3ac1084f201630d9eb', True, True, True, '2010-03-10 04:19:07', '2010-03-10 04:18:20']
 [5, u'oliver', u'', u'', u'', u'sha1$73652$3e9216708238d41669c068654e01d5658361ad03', True, True, True, '2010-03-10 04:27:43', '2010-03-10 04:24:45']
+[6, u'thomas', u'', u'', u'', u'sha1$4099a$953898f54d45d2cf02b80ecac1fe80517486c140', True, True, True, '2010-03-23 10:30:47', '2010-03-23 10:10:21']
 ]
--- dsp/datadir/dst_techchoice.py
+++ dsp/datadir/dst_techchoice.py
@@ -3,4 +3,8 @@ fields = ['id', 'session_id', 'technology_id']
 #default item format: "fieldname":("type", "value")
 default = {}
 records = [
+[1, u'7a4fd225d3e754898842f772d317ebf3', 12]
+[2, u'7a4fd225d3e754898842f772d317ebf3', 7]
+[3, u'7a4fd225d3e754898842f772d317ebf3', 2]
+[4, u'6fbf5ff1e8b65d6ce142728fcb38485a', 4]
 ]
