b92ddcebfe9197326c005a56ea0be84c8148d54f
Gabriel von Heijne <gabriel@akvo.org>
2010-03-29 09:17:11 +0000

    Data dump to capture spelling corrections

--- dsp/datadir/dst_techgroup.py
+++ dsp/datadir/dst_techgroup.py
@@ -5,7 +5,7 @@ default = {}
 records = [
 [1, u'Conveyance', 30]
 [2, u'Collection and Storage / Local Treatment', 20]
-[3, u'Toilet facilites', 10]
+[3, u'Toilet facilities', 10]
 [4, u'(Semi-) Centralised Treatment 1', 40]
 [5, u'Use of products / Disposal', 50]
 [6, u'(Semi-) Centralised Treatment 2', 45]
