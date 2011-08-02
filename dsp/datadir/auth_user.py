dece5131b1ddb03508ead40010945469ae72be8e
Mark-Tiele Westra <m.t.westra@akvo.org>
2011-08-02 15:44:51 +0200

    have run db_dump.py dump to get recent copy of database in datadir

--- dsp/datadir/auth_user.py
+++ dsp/datadir/auth_user.py
@@ -3,10 +3,11 @@ fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'is_
 #default item format: "fieldname":("type", "value")
 default = {}
 records = [
-[1, u'gabriel', u'', u'', u'g@foo.com', u'sha1$4667d$3ed2200d6f4742f70e3459cc37cba829f5659c47', True, True, True, '2010-03-29 04:08:05', '2009-12-02 17:45:52']
+[1, u'gabriel', u'', u'', u'g@foo.com', u'sha1$4667d$3ed2200d6f4742f70e3459cc37cba829f5659c47', True, True, True, '2010-10-06 16:04:18', '2009-12-02 17:45:52']
 [2, u'daniel', u'Daniel', u'Karlsson', u'daniel@fraction4.com', u'sha1$7a632$714f463e9a8bbf07be04acc56c48434e3d58e2fc', True, True, True, '2010-03-26 02:28:33', '2010-02-17 10:04:00']
-[3, u'markw', u'Mark', u'Westra', u'm.t.westra@akvo.org', u'sha1$1fff8$276d57d42241aeb72ee094d216da72d4e8232193', True, True, True, '2010-03-29 04:06:16', '2010-03-10 04:17:21']
-[4, u'bethwaaa', u'Beth', u'Whiteside', u'beth@akvo.org', u'sha1$9ec82$5819e8b452dd4f4d96aa23a6a9c455bb8d66f94c', True, True, True, '2010-03-27 01:39:34', '2010-03-10 04:18:20']
-[5, u'oliver', u'', u'', u'', u'sha1$677d1$fb45efa5ca1b573c254264e33af3bc1acfc52309', True, True, True, '2010-03-10 04:27:43', '2010-03-10 04:24:45']
-[6, u'thomas', u'', u'', u'', u'sha1$aef61$c018f54f12571f280260de70b275fb2a6dad4608', True, True, True, '2010-03-26 02:54:05', '2010-03-23 10:10:21']
+[3, u'markw', u'Mark', u'Westra', u'm.t.westra@akvo.org', u'sha1$1fff8$276d57d42241aeb72ee094d216da72d4e8232193', True, True, True, '2010-05-11 09:08:21', '2010-03-10 04:17:21']
+[4, u'bethwaaa', u'Beth', u'Whiteside', u'beth@akvo.org', u'sha1$17ef3$cee0a1dc0cb0c233e8f89ac250cf3b2e37c8ef61', True, True, True, '2010-10-06 16:06:18', '2010-03-10 04:18:20']
+[5, u'oliver', u'', u'', u'', u'sha1$5830f$3e9e9f9a1a88dc421c124fca81522781f87b1606', True, True, True, '2010-03-29 14:04:46', '2010-03-10 04:24:45']
+[6, u'thomas', u'', u'', u'', u'sha1$aef61$c018f54f12571f280260de70b275fb2a6dad4608', True, True, True, '2010-03-29 05:42:59', '2010-03-23 10:10:21']
+[7, u'sharada', u'', u'', u'', u'sha1$d6a8d$cd11d06c52b116e1bcfe9a662e38a8a85ef31802', True, True, False, '2010-05-21 04:14:53', '2010-05-21 04:13:46']
 ]
